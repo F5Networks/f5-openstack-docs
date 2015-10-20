6c2cdfc993ec2c375e23aca17a33ea655f3900e2
Jodie Putrino <j.putrino@f5.com>
2015-10-20 11:33:52 -0600

    add new pre-deploy script

---
+++ script/ci_pre-deploy.sh
@@ -0,0 +1,23 @@
+if [ "$TRAVIS_REPO_SLUG" == "jputrino/f5-openstack-docs" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing site to S3...\n"
+
+  cp -R f5-openstack-docs/ $HOME/site_build-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=site https://${GH_TOKEN}@github.com/jputrino/f5-openstack-docs site > /dev/null
+
+  cd site
+  git rm -rf ./site_build
+  cp -Rf $HOME/site_build-latest ./site_build
+  git add -f .
+  git commit -m "Lastest site build on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to site branch"
+  git push -fq origin site > /dev/null
+
+  echo -e "Published site to GitHub.\n"
+  
+fi
+
+#Hat-tip to Ben Limmer @http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/, without whose help (via the web) I never would have figured this out.
