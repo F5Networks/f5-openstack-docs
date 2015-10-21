e62c1908de7f76c265c2a55f6033ff5e0528b5ed
Jodie Putrino <jputrino@users.noreply.github.com>
2015-10-21 11:11:57 -0600

    Update cibuild.sh
    What I changed:
    - replaced http clone url with SSH clone url

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -14,7 +14,7 @@ cp -R ./site_build $HOME/site_build-latest
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=site https://$GH_USER@github.com/$TRAVIS_REPO_SLUG site
+  git clone --quiet --branch=site git@github.com:$TRAVIS_REPO_SLUG.git site
 
   cd site
   git rm -rf ./site_build
