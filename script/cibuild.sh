dacb1a7f9ab0bd01cdc188d3e24477e6bd2bde82
Jodie Putrino <j.putrino@f5.com>
2015-11-06 14:30:05 -0700

    Fixes: Website maintenance
    Problem: Jekyll needs 'index.html' to build the site. We're using 'index.html' as the login page - stored in the s3 bucket for security purposes - so we need to change the name of the file in the build before deploying to the s3 dev bucket.
    
    Analysis:
    - edited the cibuild script to rename index.html to os_landingpage.html after copying files over to travis' home dir
    
    Unit/Robot tests:

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -4,7 +4,13 @@ set -e # halt script on error
 echo "building site with jekyll"
 bundle exec jekyll build -d ./site_build
 
+# Check the html and validate links
 #echo "proofing site with htmlproofer"
 #bundle exec htmlproof ./site_build
 
 cp -R ./site_build $HOME/site_build
+
+# If this is a development build, rename index.html to os_landingpage.html
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+mv index.html os_landingpage.html
+fi
