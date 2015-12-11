b0697adb929194f031f1fa52a54e772534f17727
Jodie Putrino <j.putrino@f5.com>
2015-12-11 15:04:09 -0700

    modified deploy script
    add git config to deploy script
    
    added note to deploy script about what has to be done to fix it
    
    Fixes: update deploy script
    
    Problem:
    
    Analysis:
    
    Unit/Robot tests:
    
    update .travis.yml
    modified deploy script
    updated site url and base url in _config.yml
    added gems
    updated gems
    edit docs_page layout
    update _config.yml
    update .travis.yml
    added custom var for suburl1
    update .travis.yml
    update _config.yml
    update permissions for script
    
    update .travis.yml
    modified deploy script
    updated site url and base url in _config.yml
    added gems
    updated gems
    edit docs_page layout
    update _config.yml
    update .travis.yml
    
    added custom var for suburl1
    
    update _config.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    update permissions for script

--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -8,41 +8,41 @@
 #
 
 #  Push content up to gh-pages branch in the f5-openstack-docs repo in GitHub
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
 
-if [ "$TRAVIS_REPO_SLUG" == "F5Networks/*" ] ; then
+#if [ "$TRAVIS_REPO_SLUG" == "F5Networks/*" ] ; then
+#
+#  echo -e "Publishing docs to GitHub Pages"
+#
+#    cd $HOME
+#  git clone --quiet --branch=gh-pages git@github.com:F5Networks/f5-openstack-docs.git gh-pages > /dev/null
+#
+#  cd gh-pages
+#  git rm -rf ./site_build
+#  cp -Rf $HOME/site_build ./site_build
+#  git add -f .
+#  git commit -m "Lastest doc set on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+#  git push -fq origin gh-pages > /dev/null
+#
+#  echo -e "Published docs to F5 Networks GH Pages.";
+
+#if [ "$TRAVIS_REPO_SLUG" == "jputrino/*" ] ; then
 
   echo -e "Publishing docs to GitHub Pages"
-
-    cd $HOME
-  git clone --quiet --branch=gh-pages git@github.com:F5Networks/f5-openstack-docs.git gh-pages > /dev/null
-
-  cd gh-pages
-  git rm -rf ./site_build
-  cp -Rf $HOME/site_build ./site_build
-  git add -f .
-  git commit -m "Lastest doc set on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
-
-  echo -e "Published docs to F5 Networks GH Pages.";
-
-if [ "$TRAVIS_REPO_SLUG" == "jputrino/*" ] ; then
-
-  echo -e "Publishing docs to GitHub Pages"
-
-  cp -R build/docs/javadoc $HOME/javadoc-latest
 
   cd $HOME
-  git clone --quiet --branch=gh-pages git@github.com:jputrino/f5-openstack-docs.git gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages git@github.com:jputrino/f5-openstack-docs.git gh-pages
 
   cd gh-pages
-  git rm -rf ./site_build
-  cp -Rf $HOME/site_build ./site
+  #git rm -rf # needs to remove all content except git config dirs/files
+  cp -Rf $HOME/site_build .
   git add -f .
   git commit -m "Lastest doc set on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -f origin gh-pages
 
   echo -e "Published docs to jputrino GH Pages.";
 
 
 
-fi
+# fi
