38b8607713082111b7af115888b8bc9b305efa7b
Jodie Putrino <j.putrino@f5.com>
2015-12-16 13:20:53 -0700

    updating deploy script -- only deploy to F5Networks/gh-pages on successful pull request builds

--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -7,31 +7,21 @@
 #  Modified by Jodie Putrino on 12/11/15.
 #
 
-#  Push content up to gh-pages branch in the f5-openstack-docs repo in GitHub
+# turn tracing on so we can see the script run in travis
+set -x
+
+#  Set travis' username and email for GitHub
 git config --global user.email "OpenStack_TravisCI@f5.com"
 git config --global user.name "f5-travisci"
 
+# Log in to GitHub using travis' gh token
 travis login --pro -u $TRAVIS_USER --github-token $TRAVIS_GHTOKEN
 
-#if [ "$TRAVIS_REPO_SLUG" == "F5Networks/*" ] ; then
-#
-#  echo -e "Publishing docs to GitHub Pages"
-#
-#    cd $HOME
-#  git clone --quiet --branch=gh-pages git@github.com:F5Networks/f5-openstack-docs.git gh-pages > /dev/null
-#
-#  cd gh-pages
-#  git rm -rf ./site_build
-#  cp -Rf $HOME/site_build ./site_build
-#  git add -f .
-#  git commit -m "Lastest doc set on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-#  git push -fq origin gh-pages > /dev/null
-#
-#  echo -e "Published docs to F5 Networks GH Pages.";
+# If working in 'jputrino' repo, don't publish to GH Pages (can view site in s3 dev bucket)
 
 #if [ "$TRAVIS_REPO_SLUG" == "jputrino/*" ] ; then
 
-  echo -e "Publishing docs to GitHub Pages"
+  echo "skipping deploy script and not publishing to GitHub Pages"
 
   cd $HOME
   git clone --verbose --branch=gh-pages git@github.com:jputrino/f5-openstack-docs.git gh-pages
@@ -46,6 +36,22 @@ travis login --pro -u $TRAVIS_USER --github-token $TRAVIS_GHTOKEN
   git push -f origin gh-pages
 
   echo -e "Published docs to GH Pages.";
+
+#if [ "$TRAVIS_REPO_SLUG" == "F5Networks/*" ] && [ "$TRAVIS_PULL_REQUEST" == "TRUE" ]; then
+#
+#  echo -e "Publishing docs to GitHub Pages"
+#
+#  cd $HOME
+#  git clone --verbose --branch=gh-pages git@github.com:F5Networks/f5-openstack-docs.git gh-pages
+#
+#  cd gh-pages
+#  git rm -rf ./site_build
+#  cp -Rf $HOME/site_build ./
+#  git add -f .
+#  git commit -m "Lastest doc set on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+#  git push origin gh-pages
+#
+#  echo -e "Published docs to F5 Networks GH Pages.";
 
 # fi
 
