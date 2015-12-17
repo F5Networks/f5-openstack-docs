6534e53a3acff87b45081f3914dd55b8e7ad5ade
Jodie Putrino <j.putrino@f5.com>
2015-12-17 11:50:11 -0700

    testing changes to the deploy script

--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -14,7 +14,7 @@ git config --global user.email "OpenStack_TravisCI@f5.com"
 git config --global user.name "f5-travisci"
 
 # Log in to GitHub using travis' gh token
-travis login --pro -u $TRAVIS_USER --github-token $TRAVIS_GHTOKEN
+# travis login --pro -u f5-travisci --github-token 53c54a12d1c7ef2c22bcc755266df939b6666626
 
 if [[ "$TRAVIS_REPO_SLUG" == "F5Networks/f5-openstack-docs" ]]; then
 
