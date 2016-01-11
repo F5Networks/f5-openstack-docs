a15267a1527170cd0323ff2fd1d4473abd22fc00
mattgreene <mattgreene@users.noreply.github.com>
2016-01-11 13:32:55 -0700

    Update deploy_script.sh

--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -2,31 +2,42 @@
 #  deploy_script.sh
 #  
 #
-#  Based on a script created by blimmer for ReadyTalk -- http://github.com/ReadyTalk/swt-bling
+#  Originally based on a script created by blimmer for ReadyTalk -- http://github.com/ReadyTalk/swt-bling
 #
 #  Modified by Jodie Putrino on 12/11/15.
+#  Rewritten by Matt Greene on 01/11/16.
 #
 
-set -ev
-
-#  Set travis' username and email for GitHub
-git config --global user.email "OpenStack_TravisCI@f5.com"
-git config --global user.name "f5-travisci"
+set -e # exit with nonzero exit code if anything fails
 
 if [[ "$TRAVIS_REPO_SLUG" == "F5Networks/f5-openstack-docs" ]]; then
 
-  echo "Publishing docs to GitHub Pages"
-  cd "$HOME"
-  git clone --verbose --branch=gh-pages https://f5-travisci:$TRAVIS_PATOKEN@github.com/F5Networks/f5-openstack-docs.git gh-pages
-  cd gh-pages
+  # go to the out directory and create a *new* Git repo
+  cd "$HOME"/site_build
+  git init
+
+  # inside this git repo we'll pretend to be a new user
+  git config user.name "f5-travisci"
+  git config user.email "OpenStack_TravisCI@f5.com"
+
+  # The first and only commit to this new Git repo contains all the
+  # files present with the commit message "Deploy to GitHub Pages".
+  git add .
+  git commit -m "Deploy to GitHub Pages"
+
   git remote rm origin
   git remote add origin https://f5-travisci:$TRAVIS_PATOKEN@github.com/F5Networks/f5-openstack-docs.git
-  cp -Rf "$HOME"/site_build ./
-  git add -f .
-  git commit -m "Latest doc set auto-pushed to gh-pages on successful travis build $TRAVIS_BUILD_NUMBER"
-  git push --verbose origin gh-pages
-  echo "Published docs to F5 Networks GH Pages." else
+  
+  # Force push from the current repo's master branch to the remote
+  # repo's gh-pages branch. (All previous history on the gh-pages branch
+  # will be lost, since we are overwriting it.) We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://f5-travisci:$TRAVIS_PATOKEN@github.com/F5Networks/f5-openstack-docs.git" origin master:gh-pages-test > /dev/null 2>&1
 
- echo "Docs not published to GitHub Pages"
+  echo "Published docs to F5 Networks GH Pages."
+
+else
+
+  echo "Docs not published to GitHub Pages"
 
 fi
