1648f38c71edeed8aaa246ee5dee4b2dfa52a9c8
mattgreene <mattgreene@users.noreply.github.com>
2016-01-11 13:32:49 -0700

    What issues does this address?
    Fixes #33
    
    What's this change do?
    
    Add webhook to deploy to gh-pages after pull request is merged
    
    Where should the reviewer start?
    
    Look at scripts/deploy_script.sh which now takes the following action:
    - commit to master => deploy generate site to gh-pages (live github pages site)
    - merge request => deploy to S3 bucket, per merge request, to allow pre-accept review of changes
    - commit/pull event to anywhere else => /dev/null
    
    Any background context?
    
    Travis-CI applies the native 'deploy' config in .travis.yml upon a pull request (creation) event.  We want to stage changes for review and only deploy to production upon 'acceptance'.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 set -e # halt script on error
+set -x # enable verbose output for debugging
+
+#  Specify the git branch receiving a pull request that will replace the published docs on github pages.
+#  master is correct for final released 'images', feature.autodocgen (or something else) for testing changes
+F5_ORIGIN_BRANCH="master"
+
+
+
+if [[ "$TRAVIS_REPO_SLUG" == "F5Networks/f5-openstack-docs" && "$TRAVIS_BRANCH" == "F5_ORIGIN_BRANCH" ]]; then
+  JEKYLL_CONFIG="_config.yml"
+  JEKYLL_BASEURL="f5-openstack-docs"
+else
+  JEKYLL_CONFIG="_config_dev.yml"
+  JEKYLL_BASEURL="$TRAVIS_BUILD_NUMBER"
+fi
+
+sed -i "s/baseurl:.*$/baseurl: \"\/$JEKYLL_BASEURL\"/" $JEKYLL_CONFIG
+grep baseurl $JEKYLL_CONFIG
 
 echo "building site with jekyll"
 bundle exec jekyll build -d ./site_build
@@ -8,5 +26,5 @@ bundle exec jekyll build -d ./site_build
 echo "proofing site with htmlproofer"
 bundle exec htmlproof ./site_build --empty-alt-ignore true --disable-external true 
 
+rm -rf $HOME/site_build
 cp -R ./site_build $HOME/site_build
-
--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -1,32 +1,94 @@
 #!/bin/bash
 #  deploy_script.sh
-#  
 #
-#  Based on a script created by blimmer for ReadyTalk -- http://github.com/ReadyTalk/swt-bling
+#
+#  Originally based on a script created by blimmer for ReadyTalk -- http://github.com/ReadyTalk/swt-bling
 #
 #  Modified by Jodie Putrino on 12/11/15.
+#  Rewritten by Matt Greene on 01/11/16.
 #
 
-set -ev
+set -e # exit with nonzero exit code if anything fails
+# set -x # enable verbose output for debugging
 
-#  Set travis' username and email for GitHub
-git config --global user.email "OpenStack_TravisCI@f5.com"
-git config --global user.name "f5-travisci"
+#  Specify the git branch receiving a pull request that will replace the published docs on github pages.
+#  master is correct for final released 'images', feature.autodocgen (or something else) for testing changes
+F5_ORIGIN_BRANCH="master"
 
-if [[ "$TRAVIS_REPO_SLUG" == "F5Networks/f5-openstack-docs" ]]; then
+#  Set the git branch destination to which docs will be pushed.
+#  gh-pages is the live site for github pages, gh-pages-test is a branch for testing plumbing changes
+F5_GHPAGES_BRANCH="gh-pages"
 
-  echo "Publishing docs to GitHub Pages"
-  cd "$HOME"
-  git clone --verbose --branch=gh-pages https://f5-travisci:$TRAVIS_PATOKEN@github.com/F5Networks/f5-openstack-docs.git gh-pages
-  cd gh-pages
-  git remote rm origin
+F5_SITE_FILES="$HOME/site_build"
+
+S3_KEY="$AWS_KEY"
+S3_SECRET="$AWS_ACCESSKEY"
+S3_BUCKET='f5-openstack-dev'
+S3_URL="http://$S3_BUCKET.s3-website-us-west-2.amazonaws.com"
+
+if [[ "$TRAVIS_REPO_SLUG" != "F5Networks/f5-openstack-docs" ]]; then
+  echo "Docs not published"
+  exit
+fi
+
+function putS3 {
+  path=$1
+  file=$2
+  aws_path=$3
+  bucket=$S3_BUCKET
+  url=$S3_URL
+  date=$(date +"%a, %d %b %Y %T %z")
+  acl="x-amz-acl:public-read"
+  content_type='application/x-compressed-tar'
+  string="PUT\n\n$content_type\n$date\n$acl\n/$bucket$aws_path$file"
+  signature=$(echo -en "${string}" | openssl sha1 -hmac "${S3_SECRET}" -binary | base64)
+  curl -X PUT -T "$path/$file" \
+    -H "Host: $S3_URL" \
+    -H "Date: $date" \
+    -H "Content-Type: $content_type" \
+    -H "$acl" \
+    -H "Authorization: AWS ${S3_KEY}:$signature" \
+    "$url$aws_path$file"
+}
+
+if [[ "$TRAVIS_BRANCH" == "$F5_ORIGIN_BRANCH" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  # This is a commit to master branch, which means we update official website.
+
+  # go to the out directory and create a *new* Git repo
+  cd "$F5_SITE_FILES"
+  git init
+
+  # inside this git repo we'll pretend to be a new user
+  git config user.name "f5-travisci"
+  git config user.email "OpenStack_TravisCI@f5.com"
+
+  # The first and only commit to this new Git repo contains all the
+  # files present with the commit message "Deploy to GitHub Pages".
+  git add .
+  git commit -m "Deploy to GitHub Pages"
+
   git remote add origin https://f5-travisci:$TRAVIS_PATOKEN@github.com/F5Networks/f5-openstack-docs.git
-  cp -Rf "$HOME"/site_build ./
-  git add -f .
-  git commit -m "Latest doc set auto-pushed to gh-pages on successful travis build $TRAVIS_BUILD_NUMBER"
-  git push --verbose origin gh-pages
-  echo "Published docs to F5 Networks GH Pages." else
 
- echo "Docs not published to GitHub Pages"
+  # Force push from the current repo's master branch to the remote
+  # repo's gh-pages branch. (All previous history on the gh-pages branch
+  # will be lost, since we are overwriting it.) We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet  origin master:$F5_GHPAGES_BRANCH > /dev/null 2>&1
+
+  echo "Published docs to F5 Networks GH Pages."
+
+elif [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+
+  # This is a pull request, push to S3 for code review.
+
+  for file in "$F5_SITE_FILES"/*; do
+    putS3 "$F5_SITE_FILES" "${file##*/}" "/$TRAVIS_PULL_REQUEST"
+  done
+
+  echo "Published docs to F5 S3 Dev Bucket.  Browse to ${S3_URL}/$TRAVIS_PULL_REQUES for review."
+
+else
+
+  echo "Docs not published to GitHub Pages"
 
 fi
