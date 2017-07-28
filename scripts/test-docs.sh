46ba848440dc444fb86cb55e4d740bede663c185
Jodie Putrino <j.putrino@f5.com>
2017-07-28 15:20:33 -0600

    update test script

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-set -x
+html="make -C docs html"
+linkcheck=$(make -C docs linkcheck | grep 'broken')
+grammar="write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --passive --so --no-illusion --thereIs --cliches"
 
+if [[ $TRAVIS != "" ]]; then
+   OUTPUT=$TRAVIS_BUILD_DIR/_build/linkcheck/output.txt
+else
+   OUTPUT=docs/_build/linkcheck/output.txt
+fi
+
+goodjob() {
+  printf "%s\n" "$*" >&2
+
+}
+
+echo "Installing project dependencies"
+pip install --user -r requirements.txt
+
+set -x
 set -e
 
-echo "Building docs and checking links with Sphinx"
-make -C docs clean
-make -C docs html
-make -C docs linkcheck
-
+echo "Building docs with Sphinx"
+$html
 
 echo "Checking grammar and style"
-write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --passive --so --no-illusion --thereIs --cliches
+$grammar
+
+[[ $grammar = "" ]] || goodjob "CONGRATULATIONS! You are a grammar wizard."
+
+set +x
+set +e
+echo "Checking links"
+if [[ $linkcheck == "" ]]; then
+   echo "Linkcheck succeeded"
+else
+   echo "WARNING: linkcheck failed. Fix the following broken links:"
+   grep 'broken' $OUTPUT
+fi
